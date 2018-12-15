3e1770522f38ecf0bbe2ef968668c55fc74195ac
SM Mashuque <smjim1998@gmail.com>
2018-12-15 16:27:14 -0500

    prod test 28

--- p_apps/celery.py
+++ p_apps/celery.py
@@ -5,20 +5,13 @@ from datetime import timedelta
 from celery import Celery
 from p_apps import settings
 
-BROKER_POOL_LIMIT = 1
+# BROKER_POOL_LIMIT = 1
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'p_apps.settings')
 
 app = Celery('p_apps')
-broker_url = os.environ.get('CLOUDAMQP_URL', '')
-broker_pool_limit = 1 # Will decrease connection usage
-broker_heartbeat = None # We're using TCP keep-alive instead
-broker_connection_timeout = 30 # May require a long timeout due to Linux DNS timeouts etc
-result_backend = None # AMQP is not recommended as result backend as it creates thousands of queues
-event_queue_expires = 60 # Will delete all celeryev. queues without consumers after 1 minute.
-worker_prefetch_multiplier = 1 # Disable prefetching, it's causes problems and doesn't help performance
-worker_concurrency = 50
+
 # app = Celery('mysite', broker='pyamqp://guest@localhost//')
 # app = Celery('p_apps', broker=os.environ['REDIS_URL'])
 # app.conf.update(BROKER_URL=os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost//'),
