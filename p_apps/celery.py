7cc1dc3553cbc80377188f5901183f5a7ef0fe2a
SM Mashuque <smjim1998@gmail.com>
2018-12-14 23:41:13 -0500

    add redis in requirements.

--- p_apps/celery.py
+++ p_apps/celery.py
@@ -5,7 +5,9 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'p_apps.settings')
 
-app = Celery('p_apps')
+app = Celery('p_apps', broker=os.environ['REDIS_URL'])
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
