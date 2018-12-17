de112b8ce7c77367320c97bddce75fe2b6d6f155
SM Mashuque <smjim1998@gmail.com>
2018-12-16 23:35:17 -0500

    update the corpus of insignificant words.

--- text_analyzer/parse_function_words.py
+++ text_analyzer/parse_function_words.py
@@ -9,10 +9,12 @@ def generate_list_of_function_words(old_file, new_file):
     new_words = []
     with open(old_file, 'r') as read_file:
         for line in read_file:
-            sentence = read_file.readline().rstrip()
+            sentence = line.rstrip()
             words = re.findall("[a-z']+", sentence)
-            for word in words: new_words.append(word)
+            for word in words:
+                new_words.append(word)
 
+    print("len: ", len(new_words))
     with open(new_file, 'w', encoding='utf-8') as write_file:
         variable_name = "FUNCTION_WORDS = {\n"
         write_file.write(variable_name)
@@ -22,4 +24,4 @@ def generate_list_of_function_words(old_file, new_file):
         write_file.write("}\n")
 
 if __name__ == '__main__':
-    generate_list_of_function_words("function_words.txt", "function_words.py")
+    generate_list_of_function_words("function_words.txt", "corpus.py")
