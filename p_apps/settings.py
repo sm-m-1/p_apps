6091f4b285105b61c1a1d7f84e460a49fe47eed2
SM Mashuque <smjim1998@gmail.com>
2018-12-18 01:32:44 -0500

    add ssl redirect on for the website. security.

--- p_apps/settings.py
+++ p_apps/settings.py
@@ -31,6 +31,7 @@ except KeyError:
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+SECURE_SSL_REDIRECT = True
 
 ALLOWED_HOSTS = [
     'https://boiling-headland-86603.herokuapp.com/',
