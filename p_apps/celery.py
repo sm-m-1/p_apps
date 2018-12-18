7641c8209530fbc4ef482b5b9507f3b78f938015
SM Mashuque <smjim1998@gmail.com>
2018-12-17 20:00:23 -0500

    remove celery heartbeat for extra messages.

--- p_apps/celery.py
+++ p_apps/celery.py
@@ -20,7 +20,7 @@ app.conf.update(
     broker_url = os.environ.get('CLOUDAMQP_URL', 'pyamqp://guest@localhost//'),
     # broker_url = os.environ.get('REDIS_URL', 'pyamqp://guest@localhost//'),
     broker_pool_limit = 1, # Will decrease connection usage
-    # broker_heartbeat = None, # We're using TCP keep-alive instead
+    broker_heartbeat = None, # We're using TCP keep-alive instead
     # broker_connection_timeout = 30, # May require a long timeout due to Linux DNS timeouts etc
     # result_backend = 'django-cache', # AMQP is not recommended as result backend as it creates thousands of queues
     result_backend='rpc', # AMQP is not recommended as result backend as it creates thousands of queues
