b85aa3703242a9539562cbf4a84a7e55b7f85ddb
SM Mashuque <smjim1998@gmail.com>
2018-12-20 01:06:45 -0500

    add static root config settings in django

--- p_apps/settings.py
+++ p_apps/settings.py
@@ -145,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
